1259a9b7a92d803904b386c9463934684a945b94
SebBird <sebbird95@gmail.com>
2021-04-10 23:07:47 +0100

    add incr func, add Countformat

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -3,29 +3,41 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: []
   };
 
-  styles = {
-    fontSize: "10px",
-    fontWeight: "bold",
-  };
+  // constructor(){
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = () => {
+    this.setState( {count: this.state.count + 1}); 
+  }
+
   render() {
     return (
       <React.Fragment>
         <div>
-          <span style={this.styles} className="badge badge-primary m-2">
-            {this.formatCount()}
-          </span>
-          <button className="btn btn-secondary btn-sm">Increment</button>
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
         </div>
       </React.Fragment>
     );
   }
-
+  
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0 ? "warning" : "primary");
+    return classes;
+  }
+  
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
+  
 }
 
 export default Counter;
+
