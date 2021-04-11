539a7e3815d588db36b667890bb7a225df823f9c
Seb <sebbird95@gmail.com>
2021-04-11 22:17:13 +0100

    add counters component

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -2,43 +2,35 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: [],
+    value: this.props.value,
   };
 
-  // constructor(){
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
-
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
-      <React.Fragment>
-        <div>
-          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-          <button
-            onClick={() => this.handleIncrement()}
-            className="btn btn-secondary btn-sm"
-          >
-            Increment
-          </button>
-        </div>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
