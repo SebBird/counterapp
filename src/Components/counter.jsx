90dd323b6ee90438edae0df6dac30c620ec59c44
Seb <sebbird95@gmail.com>
2021-04-13 11:32:08 +0100

    add nav, add decrement btn, add src of truth

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
+          className="btn btn-secondary btn-sm m-1"
         >
           +
         </button>
-        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+        <button
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          className={this.getBtnClasses()}
+        >
+          -
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
@@ -20,6 +30,12 @@ class Counter extends Component {
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  getBtnClasses() {
+    let classes = "btn btn-secondary btn-sm m-1 ";
+    classes += this.props.counter.value === 0 ? "disabled" : "";
     return classes;
   }
 
