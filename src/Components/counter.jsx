6d5601167c5b6d150f0746ea8ade454251b0e669
SebBird <sebbird95@gmail.com>
2021-04-13 21:21:41 +0100

    fix disable button

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -3,26 +3,31 @@ import React, { Component } from "react";
 class Counter extends Component {
   render() {
     return (
-      <div>
+      <div className="row">
+        <div className="col-1">
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
         <button
           onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm m-1"
+          className="btn btn-secondary btn-sm"
         >
           +
         </button>
         <button
           onClick={() => this.props.onDecrement(this.props.counter)}
-          className={this.getBtnClasses()}
+          className="btn btn-secondary btn-sm m-2"
+          disabled={this.props.counter.value === 0 ? 'disabled' : ''}
         >
           -
         </button>
         <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
+          className="btn btn-danger btn-sm"
         >
           Delete
         </button>
+        </div>
       </div>
     );
   }
@@ -30,12 +35,6 @@ class Counter extends Component {
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.props.counter.value === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  getBtnClasses() {
-    let classes = "btn btn-secondary btn-sm m-1 ";
-    classes += this.props.counter.value === 0 ? "disabled" : "";
     return classes;
   }
 
